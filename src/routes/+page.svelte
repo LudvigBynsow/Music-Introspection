7ddfe1513c7c4440b17f0648844cbb81cfcbac08
Ludvig Bünsow <ludbns@edu.umea.se>
2026-04-17 14:53:59 +0200

    message

---
+++ src/routes/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+
+
+</script>
+
+<header> Musical Introspective </header>
+
+<main>
+
+
+<div class="tabs">
+    Start Introspection
+</div>
+
+<div class="tabs">
+    Information
+</div>
+
+<div class="tabs">
+    Leaderboard
+</div>
+
+</main>
+
+<style>
+main{
+    display: flex;
+    border: solid 5px blue;
+    border-radius: 30px;
+    height: 90vh;
+    width: 70vw;
+    align-items: center;
+    margin: auto;
+    flex-direction: column;
+    font-size: 30px;
+    
+}
+header{
+    display: flex;
+    height: fit-content;
+    width: fit-content;
+    font-size: 40px;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+}
+.tabs{
+    border: solid 5px rgb(113, 0, 113);
+    height: fit-content;
+    width: 40%;
+    min-width: fit-content;
+    align-items: center;
+    text-align: center;
+    margin-top: 15%;
+    background-color: purple;
+    transition: background 0.5s;
+}
+.tabs:hover{
+    background-color: rgb(120, 15, 218);
+}
+
+
+</style>
